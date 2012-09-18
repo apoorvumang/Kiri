244469e5540b3e33bb15200d183731a335b88819
Apoorv Umang <apoorvumang@gmail.com>
2012-09-18 06:28:18 +0530

    tst

--- try.php
+++ try.php
@@ -1,5 +1,6 @@
 <?php
 echo system("bin/sox try1.wav try1convert.flac");
+echo system("whoami");
 sleep(2);
-echo file_get_contents("./try1convert.flac");
+//echo file_get_contents("./try1convert.flac");
 ?>
