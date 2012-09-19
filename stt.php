8c3ed81b51b792f3a40897c991bfa9779c09eaed
Apoorv Umang <apoorvumang@gmail.com>
2012-09-19 19:54:28 +0530

    changed rate to 44100

--- stt.php
+++ stt.php
@@ -51,12 +51,12 @@ function speechToText($url_record)
 	$stturl = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-IN";
 	$upload = $query;//file_get_contents("./Tmpfile.flac");
 	$data = array(
-	    "Content_Type"  =>  "audio/x-flac; rate=16000",
+	    "Content_Type"  =>  "audio/x-flac; rate=44100",
 	    "Content"       =>  $upload,
 	);
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $stturl);
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array("Content-Type: audio/x-flac; rate=16000"));
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, array("Content-Type: audio/x-flac; rate=44100"));
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 	ob_start();
