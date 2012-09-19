4df3c0f2c99344de4d7a5b25616087b2664cb188
Akshay Aurora <akshayaurora@yahoo.com>
2012-09-19 15:49:46 +0530

    running shell comand

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<?php //echo "Hello World"; ?>
+<?php 
+$t = shell_exec('ls'); 
+echo $t;
+?>
 <a href="http://www.online-convert.com/"><img src="images/online-convert-logo.png" height=50 width=50 /></a>
